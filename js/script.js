3e8d8ea505f9d119cda84d27cec0e5053eb840bc
unknown <evil-nekromat@mail.ru>
2022-02-01 13:20:10 +0300

    smooth-burger

--- js/script.js
+++ js/script.js
@@ -34,3 +34,12 @@ function mouseOverCard(e) {
 function mouseLeftCard() {
   this.classList.add('mouse-out');
 }
+
+
+
+// ===============  smooth-burger  ======================
+//Using Vanilla JS
+document.querySelector(".hamburguer").addEventListener("click", function(){
+	document.querySelector(".full-menu").classList.toggle("active");
+	document.querySelector(".hamburguer").classList.toggle("close-hamburguer");
+});
